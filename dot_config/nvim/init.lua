ac5c033fad84dfebf4e2f569f05e2aa196a26e79
Isaac Williams <isaac@thewilliams.ws>
2026-01-12 14:07:17 -0500

    Add Neovim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,45 @@
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+vim.g.have_nerd_font = true
+vim.g.colorscheme = "catppuccin"
+vim.opt.colorcolumn = "80,120"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local uv = vim.uv or vim.loop
+if not uv.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
---
+++ dot_config/nvim/lua/plugins/rails.lua
@@ -0,0 +1,8 @@
+return {
+  { "tpope/vim-rails" },
+  { "tpope/vim-endwise" },
+  { "tpope/vim-bundler" },
+  { "tpope/vim-dispatch" },
+  { "tpope/vim-projectionist" },
+  { "slim-template/vim-slim", ft = { "slim" } },
+}
